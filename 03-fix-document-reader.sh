4fab06f1c9d2edddfeae0677431070519bfb21a6
Luís de Sousa <luis.de.sousa@protonmail.ch>
2016-08-09 09:24:08 +0200

    New method to set default odf reader.

--- 03-fix-document-reader.sh
+++ 03-fix-document-reader.sh
@@ -6,6 +6,7 @@
 echo "Installing Okular"
 yes | apt install okular
 
-# ToDo: set Okular as default.
+# ToDo: set Okular as default - the line below is not working...
 # Reference:
 # http://askubuntu.com/questions/805573/how-to-set-the-default-document-reader-in-ubuntu-16-04
+xdg-mime default okular.desktop application/pdf
