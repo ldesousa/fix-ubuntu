602715017b7e3b5872a09ff0a9d94df41e7ebf47
Luís de Sousa <luis.de.sousa@protonmail.ch>
2016-08-10 20:45:04 +0200

    New script for GEdit plugins.

---
+++ 08-fix-file-editor.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Author: Luís de Sousa (luis.de.sousa[@]protonmail.ch)
+# Date: 10-08-2016
+#
+
+echo "Installing GEdit plugins"
+
+yes | apt install gedit-developer-plugins gedit-plugins gedit-source-code-browser-plugin rabbitvcs-gedit
+
+# Extra: LaTeX
+# This installs texlive, takes about one hour.
+# yes | apt install gedit-latex-plugin 
+
+echo "To activate these plugins go to: Edit > Preferences > Plugins"
