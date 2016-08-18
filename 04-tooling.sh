4b34688855ee4b7acdb674ba301ef37318dcc0b7
Luís de Sousa <luis.de.sousa@protonmail.ch>
2016-08-18 10:00:13 +0200

    Replaced ClipIt for Diodon.

--- 04-tooling.sh
+++ 04-tooling.sh
@@ -3,6 +3,12 @@
 # Author: Luís de Sousa (luis.de.sousa[@]protonmail.ch)
 # Date: 01-08-2016
 
-echo | apt-add-repository ppa:freyja-dev/unity-tweak-tool-daily
+echo | add-apt-repository ppa:diodon-team/stable
 apt update
-yes | apt install htop git git-gui aptitude unity-tweak-tool clipit indicator-multiload
+yes | apt install htop git git-gui aptitude unity-tweak-tool diodon indicator-multiload
+
+echo "If Diodon is not recording the clipboard checkout this link:"
+echo "https://esite.ch/2014/08/why-does-diodon-not-save-anything-to-the-clipboard-history/"
+echo
+echo "To set up a shortcut for Diodon follow these instructions:"
+echo "https://esite.ch/2015/07/using-custom-shortcuts-of-de-as-diodon-hotkey/"
