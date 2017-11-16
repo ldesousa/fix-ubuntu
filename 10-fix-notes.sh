ad12d42ad0f2295fd253b63bf3e589604ef53512
Luís de Sousa <luis.de.sousa@protonmail.ch>
2017-11-16 09:38:51 +0100

    Fix notes script now using command line argument to set notes folder

--- 10-fix-notes.sh
+++ 10-fix-notes.sh
@@ -5,10 +5,18 @@
 
 echo "Installing notes management software."
 echo "Since the Ubuntu One service was brought offline notes must be synchronised 
-with a service like ownCloud. If you wish to use an alternative service or 
-notes location, please edit the script and modify the path in the last line."
+with a service like ownCloud. If you wish to use an alternative service please 
+install it manually before using this script."
+
+if [ $# -eq 0 ]
+  then
+    echo
+    echo "ERROR: Please provide the path to the desired Tomboy notes folder, e.g.:"
+    echo "sudo sh ./10-fix-notes.sh /home/user/ownCloud/tomboy"
+	exit
+fi
 
 yes | apt install owncloud-client
 yes | apt install tomboy
 
-echo 'TOMBOY_PATH="/home/'$USER'/owncloud/tomboy"' >> /etc/environment
+echo 'TOMBOY_PATH="'$1'"' >> /etc/environment
